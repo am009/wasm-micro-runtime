54262266bacc4849dcea15f09de78b7716d297fe
abc767234318 <767234318@qq.com>
2024-08-28 19:05:55 +0800

    build iwasm for android.

---
+++ linux-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export ANDROID_HOME=/home/obfuscator/install/android-sdk
+export ANDROID_NDK_LATEST_HOME=/home/obfuscator/install/android-sdk/ndk-bundle
+export WAMR_BUILD_TARGET=AARCH64
+
+cd product-mini/platforms/linux
+rm -rf build
+mkdir build
+cd build
+
+set -e
+
+cmake .. -D WAMR_BUILD_TARGET=AARCH64 -D CMAKE_EXPORT_COMPILE_COMMANDS=ON
+make
